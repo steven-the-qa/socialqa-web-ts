8b355a67bfde25e41529f95d297265f319e9d0a8
Steven Boutcher <boutchersj@gmail.com>
2024-12-26 13:53:31 -0600

    Remove MOCK_MODE

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,19 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { navigateToAmazon } from './utils';
 
-// Use this for development/debugging
-const MOCK_MODE = process.env.MOCK_MODE === 'true';
-
 test.describe('Product tests', () => {
   test('has the correct product title', async ({ page }) => {
-    if (MOCK_MODE) {
-      // Use stored data for development
-      await page.setContent('<div id="title">Spider-Man 2</div>');
-    } else {
-      await test.step("Navigate to product page", async () => {
-        await navigateToAmazon(page, 'spiderman2');
-      });
-    }
+    await test.step("Navigate to product page", async () => {
+      await navigateToAmazon(page, 'spiderman2');
+    });
 
     const productTitle = page.getByTestId("title");
     await expect(productTitle).toHaveText("Spider-Man 2");
